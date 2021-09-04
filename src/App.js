8f4240330e34a1216d871b5f9876674802b8d919
Hansle Lee <hssarah13@gmail.com>
2021-09-04 22:17:21 +0900

    useState example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React, { Component, useState } from "react";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [count, setCount] = useState(0);
+  const [email, setEmail] = useState("");
+
+  const updateEmail = e => {
+    const {
+      target: { value }
+    } = e;
+    setEmail(value);
+  };
+
+  return(
+    <>
+      {count}
+      <button onClick={() => setCount(count+1)}>Increment</button>
+      <button onClick={() => setCount(count-1)}>Decrement</button>
+      <input placeholder="Email" value={email} onChange={updateEmail}/>
+    </>
   );
 }
 
