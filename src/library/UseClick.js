6cdf43c2ae1c752f0ed5187fb5b0b27a1a43dbfb
Hansle Lee <hssarah13@gmail.com>
2021-09-06 20:16:07 +0900

    useClick 해결 안됨

--- src/library/UseClick.js
+++ src/library/UseClick.js
@@ -1,19 +1,42 @@
 import React, { useState, useEffect, useRef } from "react";
 
+const useClick = (onClick) => {
+    const element = useRef();
+    
+    //React 16.8v 부터는 Hook 을 조건문, 반복문, 중첩함수 내에서 호출할 수 없습니다.
+    useEffect(()=>{
+        // componentDidMount, componentDidUpdate(지금은 빈배열이라 ㄴㄴ)
+        if(typeof onClick !== "function") {
+            return;
+        }
+        
+         console.log(element);
+
+        if(element && element.current) {
+            element.current.addEventListener("click", onClick);
+        }
+
+        // element.current.addEventListener("click", onClick);
+        
+        // componentWillUnMount
+        return () => {
+            if(element && element.current){
+                element.current.removeEventListener("click", onClick);
+            }
+            // element.current.removeEventListener("click", onClick);
+        };
+    }, []);
+    return (typeof onClick !== "function") ? element : undefined;
+}
+
 function UseClick() {
-    const inputText = useRef();
-    
-    useEffect(() => {
-        //useEffect안쓰면 setTimeout함수가 너무 빨리 동작해서 inputText가 undefined로 나와서
-        //useEffect안에 그냥 넣어준것.
-        setTimeout(()=> inputText.current.focus(), 2000);
-    });
-    
+    const sayHello = () => {console.log("hello");};
+    const title = useClick(sayHello);
 
     return (
         <div className="UseClick">
             <h1>useClick</h1>
-            <input ref = {inputText} placeholder="cute kancho"/>
+            <h6 ref = {title}>change this</h6>
         </div>
   );
 }
